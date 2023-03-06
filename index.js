192315935f18ada6073d153ebda2e7ad38b43686
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-05 22:16:28 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: 'Sam',
+  streetAddress: '11 Broadway'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updateEmployee = {...employee};
+  updateEmployee[key] = value;
+  return updateEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+  const deleteEmployeeKey = {...employee};
+  delete deleteEmployeeKey[key];
+  return deleteEmployeeKey;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+};
